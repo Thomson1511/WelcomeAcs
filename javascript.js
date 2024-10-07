756d9d4090cedb1b0afa3b7d51e46f274cffe9d1
Thomson1511 <kovacstomi130@gmail.com>
2024-10-07 13:12:06 +0200

    welcome frissitve

--- javascript.js
+++ javascript.js
@@ -1,7 +1,9 @@
-function showOptions(){
-    var welcomediv = document.getElementById("welcomep");
-    var optionbtns = document.getElementById("optionbtns");
+var welcomediv = document.getElementById("welcomep");
+var optionbtns = document.getElementById("optionbtns");
+var back = document.getElementById("back");
 
+function showOptions(){
     welcomediv.style.display = "none";
     optionbtns.style.display = "flex";
+    back.style.display = "flex";
 }
