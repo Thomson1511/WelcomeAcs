3dff73207237694e51efdf50cba5266629d2579d
Thomson1511 <kovacstomi130@gmail.com>
2024-10-31 09:47:57 +0100

    style updated

--- javascript.js
+++
@@ -1,9 +0,0 @@
-var welcomediv = document.getElementById("welcomep");
-var optionbtns = document.getElementById("optionbtns");
-var back = document.getElementById("back");
-
-function showOptions(){
-    welcomediv.style.display = "none";
-    optionbtns.style.display = "flex";
-    back.style.display = "flex";
-}
